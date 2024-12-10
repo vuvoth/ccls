fbdc5b9857cd6944c9b0df9cb892d44c00ece8dc
NTTVy03 <trucvy05062003@gmail.com>
2024-12-10 22:54:53 +0700

    make public signal optional in main component

--- crates/parser/src/grammar/main_component.rs
+++ crates/parser/src/grammar/main_component.rs
@@ -9,11 +9,13 @@ pub fn main_component(p: &mut Parser) {
     p.expect(ComponentKw);
     p.expect(MainKw);
     
-    p.expect(LCurly);
-    p.expect(PublicKw);
-    p.expect(LBracket);
-    list_identity::parse(p);
-    p.expect(RBracket);
+    if p.at(LCurly) {
+        p.expect(LCurly);
+        p.expect(PublicKw);
+        p.expect(LBracket);
+        list_identity::parse(p);
+        p.expect(RBracket);
+    }
 
     p.expect(Assign);
     expression::expression(p);
