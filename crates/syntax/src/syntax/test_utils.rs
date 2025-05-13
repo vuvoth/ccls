b048d631d9791ba4733ffffbab6df116cc390f4b
Truc Vy <trucvy05062003@gmail.com>
2025-05-13 22:48:41 +0700

    refactor: extract syntax test utils into a separate test module

--- crates/syntax/src/syntax/test_utils.rs
+++ crates/syntax/src/syntax/test_utils.rs
@@ -1,17 +1,20 @@
-use crate::syntax_node::SyntaxNode;
-
 pub use rowan::{NodeOrToken, WalkEvent};
 
-fn level_str(level: u32) -> String {
-    let mut ans = String::from("");
+use crate::syntax_node::SyntaxNode;
 
-    for _i in 0..level {
-        ans.push_str("|     ");
-    }
-    ans
+#[macro_export]
+macro_rules! test_syntax {
+    ($file_path:expr, $scope: expr) => {
+        let crate_path = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+
+        let full_path = format!("{}{}", crate_path, $file_path);
+        let source = std::fs::read_to_string(full_path).expect("Should not failed");
+        let syntax = crate::syntax::syntax_node_from_source(&source, $scope);
+        insta::assert_snapshot!($file_path, view_ast(&syntax));
+    };
 }
 
-pub(crate) fn view_ast(node: &SyntaxNode) -> String {
+pub fn view_ast(node: &SyntaxNode) -> String {
     let mut level = 0;
     let mut result = String::new();
     for event in node.preorder_with_tokens() {
@@ -46,4 +49,13 @@ pub(crate) fn view_ast(node: &SyntaxNode) -> String {
         }
     }
     return result;
+}
+
+fn level_str(level: u32) -> String {
+    let mut ans = String::from("");
+
+    for _i in 0..level {
+        ans.push_str("|     ");
+    }
+    ans
 }
