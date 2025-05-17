386e216d1bf553838dfb96333f522696f15b278f
Truc Vy <trucvy05062003@gmail.com>
2025-05-17 22:39:15 +0700

    docs: update docs for pragma, include

--- crates/parser/src/grammar/include.rs
+++ crates/parser/src/grammar/include.rs
@@ -1,13 +1,15 @@
 use super::*;
 
-/**
- * in order to use code in other files, we have to
- * include them in our program
- * grammar:
- *     include "<filename>";
- * eg:
- *     include "circuit.circom";
- */
+/// In order to use code in other files, we have to
+/// include them in our program.
+///
+/// Grammar:
+/// * `include "<filename>";`
+///
+/// Example:
+/// ```ignore
+/// include "circuit.circom";
+/// ```
 pub(super) fn include(p: &mut Parser) {
     let m = p.open();
     p.expect(IncludeKw);
--- crates/parser/src/grammar/pragma.rs
+++ crates/parser/src/grammar/pragma.rs
@@ -1,12 +1,15 @@
 use super::*;
 
-/**
- * parse pragma in circom language
- * grammar:
- *      pragma circom <version>;
- * eg:
- *     pragma circom 2.0.0;
- */
+/// `pragma` ensures that the circuit is compatible with the
+/// specified compiler version.
+///
+/// Grammar:
+/// * `pragma circom <version>;`
+///
+/// Example:
+/// ```ignore
+/// pragma circom 2.0.0;
+/// ```
 pub fn pragma(p: &mut Parser) {
     let m = p.open();
     p.expect(PragmaKw);
